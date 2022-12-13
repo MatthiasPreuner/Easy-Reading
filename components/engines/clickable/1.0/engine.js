3981e1b6753c8d38c275ec17b0a1dc1ae28f4050
Matthias Preuner <matthias.preuner@gmail.com>
2022-12-13 12:33:18 +0100

    clickable icons added

--- components/engines/clickable/1.0/engine.js
+++ components/engines/clickable/1.0/engine.js
@@ -88,7 +88,7 @@ class Clickable extends base.EngineBase{
                 id : "clickable",
                 name: "Clickable",
                 description : "Mark clickable objects according to your favorite setting",
-                defaultIcon : "assets/clickable.png",
+                defaultIcon : "assets/clickable-full.png",
                 includeInDefaultProfile: true,
                 states: 2,
                 supportedLanguages: [],
