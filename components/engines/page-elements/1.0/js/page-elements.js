a7e7338cedeefc0cffe388a8565eabd79300e73a
Chris Stelzmueller <tuesd4y@protonmail.ch>
2022-12-14 20:25:32 +0100

    Add style reset and make Dialog scrollable in page-elements.js
    Signed-off-by: Chris Stelzmüller <tuesd4y@protonmail.ch>

--- components/engines/page-elements/1.0/js/page-elements.js
+++ components/engines/page-elements/1.0/js/page-elements.js
@@ -56,31 +56,34 @@ function pageElements(req, config, widget) {
     // find all clickable??
 
 
+    // language=HTML
     const layoutHTML =
-        `<div class="tabbed">
-    <input checked="checked" id="tab1" type="radio" name="tabs" />
-    <input id="tab2" type="radio" name="tabs" />
-    <input id="tab3" type="radio" name="tabs" />
+        `
+            <div class="tabbed">
+                <input checked="checked" id="tab1" type="radio" name="tabs"/>
+                <input id="tab2" type="radio" name="tabs"/>
+                <input id="tab3" type="radio" name="tabs"/>
 
-    <nav>
-        <label for="tab1">Headers</label>
-        <label for="tab2">Links</label>
-        <label for="tab3">Clickable Objects</label>
-        <label for="close" data-micromodal-close="er-structure" onclick="setTimeout(() => $('#er-structure').remove(), 300)">x</label>
-    </nav>
-    
-    <figure>
-        <div class="tab1">
-          <ul class="page-elements-list">${headers}</ul>
-        </div>
-        <div class="tab2">
-          <ul class="page-elements-list">${links}</ul>
-        </div>
-        <div class="tab3">
-          <ul class="page-elements-list">${links}</ul>
-        </div>
-    </figure>
-  </div>`
+                <nav>
+                    <label for="tab1">Headers</label>
+                    <label for="tab2">Links</label>
+                    <label for="tab3">Clickable Objects</label>
+                    <label for="close" data-micromodal-close="er-structure"
+                           onclick="setTimeout(() => $('#er-structure').remove(), 300)">x</label>
+                </nav>
+
+                <figure>
+                    <div class="tab1">
+                        <ul class="page-elements-list">${headers}</ul>
+                    </div>
+                    <div class="tab2">
+                        <ul class="page-elements-list">${links}</ul>
+                    </div>
+                    <div class="tab3">
+                        <ul class="page-elements-list">${links}</ul>
+                    </div>
+                </figure>
+            </div>`
 
     // noinspection JSUnusedLocalSymbols
     const test =
@@ -94,15 +97,73 @@ function pageElements(req, config, widget) {
 `
 
 
-    const formHTML = `<div class="modal micromodal-slide page-elements-modal" id="er-structure" aria-hidden="true">
-    <div class="modal__overlay" tabindex="-1" data-micromodal-close>
-      <div class="modal__container" role="dialog" aria-modal="true">
-        <div id="page-elements-content">
-          ${layoutHTML}
-        </div>
-      </div>
-      </div>
-  </div>`;
+    // language=HTML
+    const formHTML = `
+        <div class="modal micromodal-slide page-elements-modal" id="er-structure" aria-hidden="true">
+            <div class="modal__overlay" tabindex="-1" data-micromodal-close>
+                <div class="modal__container" role="dialog" aria-modal="true" style="overflow-y: auto">
+                    <style>
+                        /* http://meyerweb.com/eric/tools/css/reset/ 
+                           v2.0 | 20110126
+                           License: none (public domain)
+                        */
+
+                        html, body, div, span, applet, object, iframe,
+                        h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+                        a, abbr, acronym, address, big, cite, code,
+                        del, dfn, em, img, ins, kbd, q, s, samp,
+                        small, strike, strong, sub, sup, tt, var,
+                        b, u, i, center,
+                        dl, dt, dd, ol, ul, li,
+                        fieldset, form, label, legend,
+                        table, caption, tbody, tfoot, thead, tr, th, td,
+                        article, aside, canvas, details, embed,
+                        figure, figcaption, footer, header, hgroup,
+                        menu, nav, output, ruby, section, summary,
+                        time, mark, audio, video {
+                            margin: 0;
+                            padding: 0;
+                            border: 0;
+                            font-size: 100%;
+                            font: inherit;
+                            vertical-align: baseline;
+                        }
+
+                        /* HTML5 display-role reset for older browsers */
+                        article, aside, details, figcaption, figure,
+                        footer, header, hgroup, menu, nav, section {
+                            display: block;
+                        }
+
+                        body {
+                            line-height: 1;
+                        }
+
+                        ol, ul {
+                            list-style: none;
+                        }
+
+                        blockquote, q {
+                            quotes: none;
+                        }
+
+                        blockquote:before, blockquote:after,
+                        q:before, q:after {
+                            content: '';
+                            content: none;
+                        }
+
+                        table {
+                            border-collapse: collapse;
+                            border-spacing: 0;
+                        }
+                    </style>
+                    <div id="page-elements-content">
+                        ${layoutHTML}
+                    </div>
+                </div>
+            </div>
+        </div>`;
 
     $("body").append(formHTML);
     MicroModal.show('er-structure');
