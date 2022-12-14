6170bbf3b336de120e522a5c7f4ca2a2f3c83cc3
Chris Stelzmueller <tuesd4y@protonmail.ch>
2022-12-14 20:15:14 +0100

    Don't show invisible elements (e.g. cookie banner on JKU.at) in page-elements engine
    Signed-off-by: Chris Stelzmüller <tuesd4y@protonmail.ch>

--- components/engines/page-elements/1.0/js/page-elements.js
+++ components/engines/page-elements/1.0/js/page-elements.js
@@ -1,44 +1,63 @@
+// noinspection JSUnresolvedFunction,JSUnresolvedVariable
+
 function pageElements(req, config, widget) {
 
-  // reset
-  $('#er-structure').remove();
+    /**
+     * Check if the current html element is currently visible per the browser computed styles
+     * @param element {Element}
+     * @return boolean
+     */
+    function isVisible(element) {
+        const computedStyles = window.getComputedStyle(element);
 
-  // find all headers
-  var headers = Array.prototype.slice
-  .call(document.querySelectorAll("h1,h2,h3,h4,h5,h6"))
-  .map(function (element) {
+        if (computedStyles['display'] === 'none' || computedStyles['display'] === 'invisible') return false;
+        if (computedStyles['height'] == 0 && computedStyles["width"] == 0) return false;
+        if (computedStyles['opacity'] == 0) return false;
 
-    lvl = Number(element.tagName.substring(1))
+        // check if bounding rectangle has any size
+        const rect = element.getBoundingClientRect();
+        if (rect.width === 0 && rect.height === 0) return false;
 
-    return `<li><span class="page-elements-header-div header-${lvl}">${element.tagName}</span> ${element.innerHTML}</li>`
-  }).join('')
-
-  // find all links
-  var links = Array.prototype.slice
-  .call(document.querySelectorAll("a"))
-  .map(function (element) {
-    // https://stackoverflow.com/questions/2161634/how-to-check-if-element-has-any-children-in-javascript
-    if(element.innerHTML.trim() == "")
-      return ""
-    if(element.hasChildNodes()){
-      // if(element.querySelectorAll("img")){
-
-      // }
-      //if (element.firstElementChild())
-
-      // if image, remove or make smaller TODO
-      if(element.innerHTML.tagName == "img")
-      return `<li><a href=${element.href}>${element.innerHTML.alt}</a></li>`
+        return true;
     }
 
-    return `<li><a href=${element.href}>${element.innerHTML}</a></li>`
-  }).join('')
+    // reset
+    $('#er-structure').remove();
 
-  // find all clickable??
+    // find all headers
+    const headers = [...document.querySelectorAll("h1,h2,h3,h4,h5,h6").values()]
+        .filter(element => isVisible(element) && element.innerHTML.trim() !== "")
+        .map(function (element) {
+            const lvl = Number(element.tagName.substring(1))
+            return `<li><span class="page-elements-header-div header-${lvl}">${element.tagName}</span> ${element.innerHTML}</li>`
+        }).join('');
+
+    // find all links
+    const links = [...document.querySelectorAll("a").values()]
+        .filter(element => isVisible(element) && element.innerHTML.trim() !== "")
+        .map(function (element) {
+            // https://stackoverflow.com/questions/2161634/how-to-check-if-element-has-any-children-in-javascript
+            if (element.hasChildNodes()) {
+                // if(element.querySelectorAll("img")){
+
+                // }
+                //if (element.firstElementChild())
+
+                // if image, remove or make smaller TODO
+                if (element.innerHTML.tagName === "img") {
+                    return `<li><a href=${element.href}>${element.innerHTML.alt}</a></li>`
+                }
+                // fixme: question from Chris, probably to Matthias: is there an 'else' block missing?
+            }
+
+            return `<li><a href=${element.href}>${element.innerHTML}</a></li>`
+        }).join('');
+
+    // find all clickable??
 
 
-  var layoutHTML = 
-  `<div class="tabbed">
+    const layoutHTML =
+        `<div class="tabbed">
     <input checked="checked" id="tab1" type="radio" name="tabs" />
     <input id="tab2" type="radio" name="tabs" />
     <input id="tab3" type="radio" name="tabs" />
@@ -63,9 +82,9 @@ function pageElements(req, config, widget) {
     </figure>
   </div>`
 
-var test = 
-
-`<div class="micromodal-slide modal is-open" id="modal-2" aria-hidden="false">
+    // noinspection JSUnusedLocalSymbols
+    const test =
+        `<div class="micromodal-slide modal is-open" id="modal-2" aria-hidden="false">
   <div class="modal__overlay" tabindex="-1" data-custom-close="">
     <div class="modal__container w-40-ns w-90" role="dialog" aria-modal="true" aria-labelledby="modal-2-title">
     
@@ -75,7 +94,7 @@ var test =
 `
 
 
-  var formHTML = `<div class="modal micromodal-slide page-elements-modal" id="er-structure" aria-hidden="true">
+    const formHTML = `<div class="modal micromodal-slide page-elements-modal" id="er-structure" aria-hidden="true">
     <div class="modal__overlay" tabindex="-1" data-micromodal-close>
       <div class="modal__container" role="dialog" aria-modal="true">
         <div id="page-elements-content">
@@ -83,8 +102,8 @@ var test =
         </div>
       </div>
       </div>
-  </div>`        
-  
-  $("body").append(formHTML);
-  MicroModal.show('er-structure'); 
+  </div>`;
+
+    $("body").append(formHTML);
+    MicroModal.show('er-structure');
 }
