699f48a40cf7aa351bc67412c1e1ca319b193bb2
Matthias Preuner <matthias.preuner@gmail.com>
2022-12-19 12:25:31 +0100

    added translations

--- components/engines/clickable/1.0/engine.js
+++ components/engines/clickable/1.0/engine.js
@@ -6,80 +6,11 @@ class Clickable extends base.EngineBase{
         super();
         this.id = "Clickable";
         this.name = "Clickable";
-        this.description = "Mark all clickable objects in the whole web-page according to your favorite setting";
+        this.description = "Marks all clickable objects in the whole web-page according to your favorite setting";
         this.version = "1.0";
         this.debugMode = false;
         this.versionDescription = "Initial version";
 
-    }
-
-    getConfigurationDataOptions() {
-        return [
-            
-            {
-            type: "colorCombination",
-            propertyMapping: [
-                {dataSchemaProerty: "fontColor", configurableDataOptionProerty: "text-color"},
-                {dataSchemaProerty: "backgroundColor", configurableDataOptionProerty: "background-color"}
-            ],
-            configurableDataOption: [
-                {"label": "Text", "text-color": "#FFFFFF", "background-color": "#000000" },
-                {"label": "Text", "text-color": "#f9dc5c", "background-color": "#00006e" },
-                {"label": "Text", "text-color": "#333333", "background-color": "#f8f8f8"},
-                {"label" : "Text","text-color": "#0000cc", "background-color": "#FFFFFF"}
-               
-            ]
-            }
-            /*{
-                type: "colorPicker",
-                dataSchemaProerty: ["fontColor", "backgroundColor"]
-            }*/
-           /*
-            {
-                type: "singleSelectList",
-                dataSchemaProerty: ["fontColor", "backgroundColor"],
-                configurableDataOption: [
-                    {"label" : "white","value": "#FFFFFF"},
-                    {"label" : "Red","value": "#FF0000"},
-                    {"label" : "#0000FF","value": "#0000FF"},
-                    {"label" : "#00BB00","value": "#00BB00"},
-                    {"label" : "#00BB00","value": "#000000"}
-                ]
-            }
-            */
-            /*{
-                type: "text",
-                dataSchemaProerty: ["fontColor", "backgroundColor"]
-            }
-            */
-        ]
-    }
-
-    getDataSchema(){
-        return {
-            "type": "object",
-            "properties": {
-                "backgroundColor": {
-                    "title": "Background color",
-                    "description": "Preferred background color",
-                    "type": "string",
-                    "format": "color",
-                    "default": "#000000",
-                },
-                "fontColor": {
-                    "title": "Font color",
-                    "description": "Preferred font color",
-                    "type": "string",
-                    "format": "color",
-                    "default": "#FFFFFF"
-                }
-            },
-
-            "required": [
-                "backgroundColor",
-                "fontColor"
-            ]
-        };
     }
 
     getFunctions(){
@@ -87,7 +18,7 @@ class Clickable extends base.EngineBase{
             {
                 id : "clickable",
                 name: "Clickable",
-                description : "Mark clickable objects according to your favorite setting",
+                description : "Marks clickable objects.",
                 defaultIcon : "assets/clickable-light.png",
                 includeInDefaultProfile: true,
                 states: 2,
@@ -120,7 +51,7 @@ class Clickable extends base.EngineBase{
                 description:[
                     this.descriptionManager.createSubHeadingEntry(this,"intro_text","Instruction:"),
                     this.descriptionManager.createOrderedListItemEntry(this,"instruction_1","Click on the Clickable Button"),
-                    this.descriptionManager.createOrderedListItemEntry(this,"instruction_2","Color of background and text changes."), // TODO
+                    this.descriptionManager.createOrderedListItemEntry(this,"instruction_2","Color of background and text changes."),
                 ]
             }
 
